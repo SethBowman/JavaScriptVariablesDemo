035857ec030be568ff7dbd50b4332ae511d11e64
SethBowman <brandonsethbowman@gmail.com>
2023-11-05 20:41:33 -0600

    demo completed

---
+++ app.js
@@ -0,0 +1,32 @@
+//using let
+let greeting = "Hello World!"
+
+console.log(greeting)
+
+let numberOfPeople = 4
+
+console.log(numberOfPeople)
+
+//using var
+
+if(true) {
+    var language = "C#"
+}
+
+console.log(language)
+
+var classroom = 41
+
+//using const
+
+const name = "Seth"
+const birthYear = 1994
+
+//get user input
+let response = prompt("What is your name?")
+
+//alert a message to the user
+//template strings have to have the back tick string to even be used ``
+//template string uses the $ at every usage, unlike C# where you only need it once
+
+alert(`Hello ${response}!`)
